e1fb9d745abd602d42e75621d9608080593dbb1a
ManOrNot <ormanis1609@gmail.com>
2018-11-07 15:21:28 +0200

    Add files via upload

--- GPS.py
+++ GPS.py
@@ -1,26 +1,43 @@
 import serial
+import time
 
-def serial_init(port_, timeout):
+def serial_init(port_):
     if(not(isinstance(port_, str))):
         raise ValueError('port must be string')
     ser = serial.Serial(
-        port='COM20',
+        port=port_,
         baudrate = 115200,
-        timeout=timeout
+		timeout = 1
         )
 
     return(ser)
 #example
-my_ser = serial_init('COM20',1)
+my_ser = serial_init('COM4')
 
+while(True):
+	got_message = my_ser.readline().split()
+	for elem in got_message[:5]:
+		print(elem)
+	time.sleep(1)
+my_ser.close()
+'''
 x = my_ser.read()          # read one byte
 if not x:
     print('there is nothing')
+else:
+	print(x)
+	
 s = my_ser.read(10)        # read up to ten bytes (timeout)
 if not s:
     print('there is nothing')
+else:
+	print(s)
 line = my_ser.readline()   # read a '\n' terminated line
 if not line:
     print('there is nothing')
+else:
+	print(line)'''
+	
+
 
 
